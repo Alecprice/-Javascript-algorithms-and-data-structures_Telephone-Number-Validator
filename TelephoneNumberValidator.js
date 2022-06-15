8f43f55c1c4049f3bac908920d6de8aed8a9f425
Alec Price <52548136+Alecprice@users.noreply.github.com>
2022-06-15 16:16:05 -0500

    Add files via upload

---
+++ TelephoneNumberValidator.js
@@ -0,0 +1,36 @@
+var validChecker =
+  /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+function telephoneCheck(str) {
+  return validChecker.test(str);
+}
+
+telephoneCheck("555-555-5555");
+telephoneCheck("1 555-555-5555");
+telephoneCheck("1 (555) 555-5555");
+telephoneCheck("5555555555");
+telephoneCheck("555-555-5555");
+telephoneCheck("(555)555-5555");
+telephoneCheck("1(555)555-5555");
+telephoneCheck("555-5555");
+telephoneCheck("5555555");
+telephoneCheck("1 555)555-5555");
+telephoneCheck("1 555 555 5555");
+telephoneCheck("1 456 789 4444");
+telephoneCheck("123**&!!asdf#");
+telephoneCheck("55555555");
+telephoneCheck("(6054756961)");
+telephoneCheck("2 (757) 622-7382");
+telephoneCheck("0 (757) 622-7382");
+telephoneCheck("-1 (757) 622-7382");
+telephoneCheck("2 757 622-7382");
+telephoneCheck("10 (757) 622-7382");
+telephoneCheck("27576227382");
+telephoneCheck("(275)76227382");
+telephoneCheck("2(757)6227382");
+telephoneCheck("2(757)622-7382");
+telephoneCheck("555)-555-5555");
+telephoneCheck("(555-555-5555");
+telephoneCheck("(555)5(55?)-5555");
+telephoneCheck("55 55-55-555-5");
+telephoneCheck("11 555-555-5555");
